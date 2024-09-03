5a905be520d082b99f72fd524f2f37f84a7c79a7
PedroTheDevv <pedroramosgouveaandrade@gmail.com>
2024-09-03 18:31:52 -0300

    .

--- site/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.menu').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('active');
-});
-
-document.querySelector('.drop').addEventListener('click', function() {
-    document.querySelector('.drop ul').classList.toggle('active');
-});
